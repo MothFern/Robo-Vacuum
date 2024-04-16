33af9c6414024546f6aca887956d8b7b87cda9e0
savahtar <129635535+savahtar@users.noreply.github.com>
2024-04-15 18:16:03 -0600

    Add files via upload

---
+++ FinalProject_v1.2.1.ino
@@ -0,0 +1,83 @@
+#include <Servo.h>
+#include "SR04.h"
+#define TRIG_PIN 12
+#define ECHO_PIN 11
+
+SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
+long a;
+
+const int ena1 = 9;
+const int drive1A = 4;
+const int drive2A = 3;
+const int buttonPin = 5;
+int currentState;
+bool toggle = 0;
+
+Servo myservo;
+Servo myservo2;
+
+int pos = 0;
+int pos2 = 0;
+int buttonState = 0;
+int spin_speed = 0;
+
+//bool is_running = false; // motor is off to start
+bool In1State = LOW; // Direction pin 1 is LOW (off) to start
+bool In2State = LOW; // Direction pin 2 is LOW (off) to start
+
+
+void setup() {
+
+  Serial.begin(9600);
+  myservo.attach(8);
+  myservo2.attach(7);
+
+  pinMode(ena1,OUTPUT);
+  pinMode(drive1A,OUTPUT);
+  pinMode(drive2A,OUTPUT);
+
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  currentState = digitalRead(buttonPin);
+  a = sr04.Distance();
+  Serial.print(a);
+  Serial.println("cm");
+
+  if(currentState == LOW){
+    toggle = !toggle;
+    delay(300);
+    
+    if(toggle == 0){
+      Serial.println(a);
+      if(a<7){
+        In1State = HIGH; In2State = LOW; spin_speed = 255;
+        digitalWrite(drive1A, 255);  
+        digitalWrite(drive2A,255); 
+        analogWrite(ena1,0);
+      }else{
+        In1State = HIGH; In2State = LOW; spin_speed = 255;
+        digitalWrite(drive1A, In1State);  
+        digitalWrite(drive2A,In2State); 
+        analogWrite(ena1,255);
+      }
+
+      for(pos = 0; pos <=180; pos += 1){
+        myservo.write(pos);
+        myservo2.write(pos);
+        delay(20);
+      }
+      for(pos = 180; pos >=0; pos-=1 ){
+        myservo.write(pos);
+        myservo2.write(pos);
+        delay(20);
+      }
+
+    }else{
+      digitalWrite(drive1A, LOW);  
+      digitalWrite(drive2A,LOW); 
+      analogWrite(ena1,0);
+    }
+  }
+}
